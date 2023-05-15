94ce4c04091d8ce07e99aed6a11f81abf08963c8
Jeff Cook <114430321+jcook-uptycs@users.noreply.github.com>
2023-05-15 12:52:17 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+module.exports = (app) => {
+  app.on("issues.opened", async (context) => {
+    const params = context.issue({ body: "Hello World!" });
+    await context.octokit.issues.createComment(params);
+  });
+};
