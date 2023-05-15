a7e507d56f06879a50c59c3d2d8aa1a2168ac023
Jeff Cook <114430321+jcook-uptycs@users.noreply.github.com>
2023-05-15 14:33:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 module.exports = (app) => {
+  app.log("Yay! The app was loaded!");
+  
   app.on("issues.opened", async (context) => {
     const params = context.issue({ body: "Hello World!" });
     await context.octokit.issues.createComment(params);
