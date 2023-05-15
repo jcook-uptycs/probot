3f483f506cec9dafd632bbed639f4a2e21407725
Jeff Cook <114430321+jcook-uptycs@users.noreply.github.com>
2023-05-15 11:21:17 -0700

    Update entrypoint

--- app.js
+++ app.js
@@ -3,4 +3,12 @@ module.exports = (app) => {
     const params = context.issue({ body: "Hello World!" });
     await context.octokit.issues.createComment(params);
   });
+
+  app.onAny(async (context) => {
+    context.log.info({ event: context.name, action: context.payload.action });
+  });
+
+  app.onError(async (error) => {
+    app.log.error(error);
+  });  
 };
